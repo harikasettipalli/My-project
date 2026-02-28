4a878985249709969dc84826b37a3c70a9ebed3a
harikasettipalli <harikasettipalli@gmail.com>
2026-02-28 15:00:48 +0530

    Add script.js for user greeting functionality

---
+++ script.js
@@ -0,0 +1,12 @@
+// script.js
+/**
+ * A simple JavaScript file to demonstrate uploading to GitHub.
+ * This function greets a user by name.
+ */
+function greetUser(name) {
+  return "Hello, " + name + "! Welcome to GitHub.";
+}
+
+// Example usage:
+const userName = "Developer";
+console.log(greetUser(userName));
